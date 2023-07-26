a82924856030c83cde9d2d1a8623f59ff764a967
Viktor Bonino <viktor.bonino@gmail.com>
2023-07-26 16:08:29 +0200

    fix: put analytics inside body

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         )}
       >
         {children}
+        <Analytics />
       </body>
-      <Analytics />
     </html>
   )
 }
